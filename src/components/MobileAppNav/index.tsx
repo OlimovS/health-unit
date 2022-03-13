2827aa471d2318559f9743b58bc404f1d5d88f8e
Sulaymon Olimov <Sulaymon_Olimov@epam.com>
2022-03-13 11:51:14 +0500

    improved: app navbar

--- src/components/MobileAppNav/index.tsx
+++ src/components/MobileAppNav/index.tsx
@@ -10,29 +10,27 @@ import NavItem from "./NavItem";
 
 const MobileAppNav = () => {
   return (
-    <div>
-      <div>
-        <nav className="mobile__app__nav">
-          <ul>
-            <li>
-              <NavItem icon={<AiOutlineHome />} text="Home" link="/" />
-            </li>
-            <li>
-              <NavItem icon={<MdOutlineArticle />} text="Blog" link="/blog" />
-            </li>
-            <li>
-              <NavItem
-                icon={<AiOutlineHome />}
-                text="Klinikalar"
-                link="/kliniks"
-              />
-            </li>
-            <li>
-              <NavItem icon={<FiUser />} text="Profile" link="/profile" />
-            </li>
-          </ul>
-        </nav>
-      </div>
+    <div className="app__nav__wrapper">
+      <nav className="mobile__app__nav">
+        <ul>
+          <li>
+            <NavItem icon={<AiOutlineHome />} text="Home" link="/" />
+          </li>
+          <li>
+            <NavItem icon={<MdOutlineArticle />} text="Blog" link="/blog" />
+          </li>
+          <li>
+            <NavItem
+              icon={<AiOutlineHome />}
+              text="Klinikalar"
+              link="/kliniks"
+            />
+          </li>
+          <li>
+            <NavItem icon={<FiUser />} text="Profile" link="/profile" />
+          </li>
+        </ul>
+      </nav>
     </div>
   );
 };
